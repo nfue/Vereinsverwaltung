54d1dbd30daa5c7794f6497c9a6c81ee341c47e5
Niklas <fuelix@web.de>
2023-03-23 00:34:49 +0100

    Gerüst hinzugefügt: Login, Registrieren

---
+++ Vereinsverwaltung/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Vereinsverwaltung.Data;
+
+[assembly: HostingStartup(typeof(Vereinsverwaltung.Areas.Identity.IdentityHostingStartup))]
+namespace Vereinsverwaltung.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
